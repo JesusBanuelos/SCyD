ec40246f397db9e919bf7be0fb84f3754a4dec5c
Jesus Bañuelos Gordillo <36545429+JesusBanuelos@users.noreply.github.com>
2018-05-17 16:35:40 -0500

    Update tabla.php

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,11 +2,11 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]">X</button>');
+ $cols = array('id',  'nombre',  'fecha', 'Acciones'=>'<button class="borrar" data="@[0]"><img src="https://cdn170.picsart.com/upscale-231640123039212.png?r1024x1024"></button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
  
- $props = array('nombre'=>array('class'=>'busca'), 'fecha'=>'class="busca"');
+$props = array('id'=>array('class'=>'busca'));
  
  HTML::tabla($liga, 'Usuarios registrados', $cols, $props);
 ?>
